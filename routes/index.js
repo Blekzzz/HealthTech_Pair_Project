75f5cb21dade7c71e1facc5f52a358b275f7f3a3
NathanzaGaurangga <nathnzagaurangga@gmail.com>
2023-06-08 07:54:45 +0800

    install package

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('home')
+})
+
+router.get('/login', (req, res) => {
+    res.render('loginPage')
+})
+
+router.get('/register', (req, res) => {
+    res.render('registerPage')
+})
+
+module.exports = router
